ec8da08d2f2952457d99683e896841eb3576a4ac
Nicolas Peralta <nperaltapz@gmail.com>
2024-01-08 05:26:50 -0500

    scraping into description pages 2

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -9,11 +9,14 @@ class BookspiderSpider(scrapy.Spider):
     def parse(self, response):
         books = response.css("article.product_pod")
         for book in books:
-            yield{
-                "name": book.css('h3 a::text').get(),
-                "price": book.css('.product_price .price_color::text').get(),
-                "url": book.css('h3 a').attrib['href']
-            }
+            relative_url = book.css('h3 a ::attr(href)').get()
+
+            if ('catalogue/') in relative_url:
+                book_url = "https://books.toscrape.com/" + relative_url
+            else:
+                book_url = "https://books.toscrape.com/catalogue/" + relative_url
+            yield response.follow(book_url, callback=self.parse_book_page)
+        
         next_page = response.css("li.next a ::attr(href)").get()
 
         if next_page is not None:
@@ -21,7 +24,7 @@ class BookspiderSpider(scrapy.Spider):
                 next_page_url = "https://books.toscrape.com/" + next_page
             else:
                 next_page_url = "https://books.toscrape.com/" + "catalogue/" +next_page
-            yield response.follow(next_page_url, callback= self.parse)
+            yield response.follow(next_page_url, callback= self.parse_book_page)
 
     def parse_book_page(self, response):
 
