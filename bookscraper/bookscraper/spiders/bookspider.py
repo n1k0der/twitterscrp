c243565169bb539974633f1f51fa37f7ddc02de5
Nicolas Peralta <nperaltapz@gmail.com>
2024-01-08 05:35:12 -0500

    scraping into description pages bugs corrected

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -11,20 +11,19 @@ class BookspiderSpider(scrapy.Spider):
         for book in books:
             relative_url = book.css('h3 a ::attr(href)').get()
 
-            if ('catalogue/') in relative_url:
+            if 'catalogue/' in relative_url:
                 book_url = "https://books.toscrape.com/" + relative_url
             else:
                 book_url = "https://books.toscrape.com/catalogue/" + relative_url
             yield response.follow(book_url, callback=self.parse_book_page)
         
         next_page = response.css("li.next a ::attr(href)").get()
-
         if next_page is not None:
-            if next_page.startswith("catalogue"):
+            if 'catalogue/' in next_page:
                 next_page_url = "https://books.toscrape.com/" + next_page
             else:
-                next_page_url = "https://books.toscrape.com/" + "catalogue/" +next_page
-            yield response.follow(next_page_url, callback= self.parse_book_page)
+                next_page_url = "https://books.toscrape.com/catalogue/" + next_page
+            yield response.follow(next_page_url, callback= self.parse)
 
     def parse_book_page(self, response):
 
@@ -40,7 +39,7 @@ class BookspiderSpider(scrapy.Spider):
             'availability': table_rows[5].css('td::text').get(),
             'num_reviews': table_rows[6].css('td::text').get(),
             'stars': response.css("p.star-rating").attrib['class'],
-            'category': response.xpath("//ul[@class='breadcrumb']/li[@class='active]/preceding-sibling::li[1]/a/text()").get(),
+            'category': response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
             'description': response.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
             'price': response.css('p.price_color ::text').get()
 
